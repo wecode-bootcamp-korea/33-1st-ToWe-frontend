329e04bd1dff7f52753cbf492ff7ad30e2125fec
dbwlsgur9811 <dbwlsgur9811@gmail.com>
2022-05-24 11:12:13 +0900

    ADD : Nav,Footer Component 추가

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import './Nav.scss';
 
 const Nav = () => {
-  return <div>ddd</div>;
+  return (
+    <div className="Nav">
+      <div className="title">
+        <div className="bigTitle">To We</div>
+        <div className="smallTitle">ㅡㅡㅡㅡㅡㅡstudioㅡㅡㅡㅡㅡㅡ</div>
+      </div>
+      <div className="category">
+        <span className="teamInfor">팀이름</span>
+        <span className="store">스토어</span>
+        <span>게시판</span>
+        <span>나중에 추가</span>
+      </div>
+      <div className="navIcons">
+        <i class="fa-solid fa-magnifying-glass" />
+        <i class="fa-solid fa-user" />
+        <i class="fa-solid fa-cart-shopping" />
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
