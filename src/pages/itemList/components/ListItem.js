c96ce552fd4efa75753f8bf1039c7221aff32b83
Hengxi <jinhengxi93@gmail.com>
2022-05-24 15:04:52 +0900

    hover img

--- src/pages/itemList/components/ListItem.js
+++ src/pages/itemList/components/ListItem.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import './ListItem.scss';
 import ListItemContenst from './ListItemContenst.js';
 
 const ListItem = ({ listItems }) => {
+  const [tapId, setTapId] = useState(1);
+
+  const clickHandler = id => {
+    setTapId(id);
+  };
+
   return (
     <div className="listItem">
       {listItems.map(listItem => (
--- src/pages/itemList/components/ListItemContenst.js
+++ src/pages/itemList/components/ListItemContenst.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import './ListItemContenst.scss';
 
 const ListItemContenst = ({ listItem }) => {
-  const { name, price, itemImg } = listItem;
+  const { name, price, itemImg, hoverImg } = listItem;
+  const [changeImg, setChangeImg] = useState(true);
+
+  const mouseOver = () => {
+    setChangeImg(false);
+  };
+  const mouseOut = () => {
+    setChangeImg(true);
+  };
+
   return (
-    <div className="listItems">
-      <img className="listItemsImg" alt="Items" src={itemImg} />
+    <div className="listItems" onMouseOver={mouseOver} onMouseOut={mouseOut}>
+      {changeImg ? (
+        <img className="listItemsImg" alt="Items" src={itemImg} />
+      ) : (
+        <img className="listItemsImg" alt="Items" src={hoverImg} />
+      )}
+
       <p className="listItemName">{name}</p>
       <p className="listItemPrice">{price}</p>
     </div>
