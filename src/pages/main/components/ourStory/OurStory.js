84056bad948fc6aabadee5cdd34e6310f768ea59
seul <kseul06@gmail.com>
2022-05-25 20:08:09 +0900

    Modify : 오타 및 데이터 수정

--- src/pages/main/components/ourStory/OurStory.js
+++ src/pages/main/components/ourStory/OurStory.js
@@ -4,7 +4,7 @@ import './OurStory.scss';
 const OurStory = () => {
   return (
     <div className="ourStory">
-      <h2 className="title">OUR STORY</h2>
+      <h2 className="title">TOWE STORY</h2>
       <div className="description">
         [무직 ; unemployed] 직장을 벗어나 원하는 일을 하는 자유로운 삶을
         지향합니다. <br />
