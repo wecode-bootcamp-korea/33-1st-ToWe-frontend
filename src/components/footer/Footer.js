d7fc22c148c650b8915a3d54181e556ebdbbffd4
dbwlsgur9811 <dbwlsgur9811@gmail.com>
2022-05-25 19:43:56 +0900

    Modify : 기존 mastrt branch 오류 수정

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Footer.scss';
 import { FaBold, FaRegEnvelope, FaInstagram } from 'react-icons/fa';
+import './Footer.scss';
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footerIcons">
-        <FaRegEnvelope className="footerIcon" size={24} />
-        <FaInstagram className="footerIcon" size={24} />
-        <FaBold className="footerIcon" size={24} />
+        <FaRegEnvelope className="footerIcon" />
+        <FaInstagram className="footerIcon" />
+        <FaBold className="footerIcon" />
       </div>
       <span>이용약관</span>
       <span>개인정보처리방침</span>
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Nav.scss';
 import { FaSearch, FaShoppingCart, FaUserAlt } from 'react-icons/fa';
+import CategoryBar from './components/CategoryBar';
 
 const Nav = () => {
   return (
@@ -12,30 +13,22 @@ const Nav = () => {
             <div className="smallTitle">ㅡㅡㅡㅡㅡㅡ studio ㅡㅡㅡㅡㅡㅡ</div>
           </div>
           <div className="navMenu">
-            <span className="teamInfor">팀이름</span>
+            <span className="teamInfor">
+              팀이름
+              <CategoryBar />
+            </span>
             <span className="store">
               스토어
-              <div className="navDetailBar">
-                <span>팀소개</span>
-                <div className="storeCategory">
-                  {category.map(id => (
-                    <div calssName="category" key={id.id}>
-                      {id.name}
-                    </div>
-                  ))}
-                </div>
-                <div className="storeSort">
-                  {sortFilter.map(id => (
-                    <div className="bestNew" key={id.id}>
-                      {id.name}
-                    </div>
-                  ))}
-                </div>
-                <div className="duckToy" />
-              </div>
+              <CategoryBar />
             </span>
-            <span>게시판</span>
-            <span>나중에 추가</span>
+            <span className="board">
+              게시판
+              <CategoryBar />
+            </span>
+            <span className="gallery">
+              갤러리
+              <CategoryBar />
+            </span>
           </div>
           <div className="navIcons">
             <FaSearch />
@@ -49,42 +42,3 @@ const Nav = () => {
 };
 
 export default Nav;
-
-const category = [
-  {
-    id: 1,
-    name: '전체상품',
-  },
-
-  {
-    id: 2,
-    name: '레고',
-  },
-
-  {
-    id: 3,
-    name: '인형',
-  },
-
-  {
-    id: 4,
-    name: '자동차',
-  },
-
-  {
-    id: 5,
-    name: '퍼즐',
-  },
-];
-
-const sortFilter = [
-  {
-    id: 1,
-    name: '베스트 30',
-  },
-
-  {
-    id: 2,
-    name: '신상품',
-  },
-];
