b72f71661d117e519175dcfd9005e5f78d539a58
Hengxi <jinhengxi93@gmail.com>
2022-05-26 22:48:33 +0900

    modify navbar HTML,CSS 수정

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,44 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.scss';
-import { FaSearch, FaShoppingCart, FaUserAlt } from 'react-icons/fa';
-import CategoryBar from './components/CategoryBar';
+import { FaSearch, FaShoppingCart, FaUserAlt, FaBars } from 'react-icons/fa';
 
 const Nav = () => {
   return (
-    <div className="nav">
-      <div className="navBar">
-        <div className="navBasic">
-          <div className="title">
-            <div className="bigTitle">To We</div>
-            <div className="smallTitle">ㅡㅡㅡㅡㅡㅡ studio ㅡㅡㅡㅡㅡㅡ</div>
-          </div>
-          <div className="navMenu">
-            <span className="teamInfor">
-              팀이름
-              <CategoryBar />
-            </span>
-            <span className="store">
-              스토어
-              <CategoryBar />
-            </span>
-            <span className="board">
-              게시판
-              <CategoryBar />
-            </span>
-            <span className="gallery">
-              갤러리
-              <CategoryBar />
-            </span>
-          </div>
-          <div className="navIcons">
-            <FaSearch />
-            <FaUserAlt />
-            <FaShoppingCart />
-          </div>
+    <nav>
+      <div className="navbar">
+        <div className="navbarMenu">
+          <h1 className="navbarTiele">ToWeStory</h1>
+          <ul className="navbarMenuItems">
+            <li className="navbarMenuItem">ToWe</li>
+            <li className="navbarMenuItem">스토어</li>
+            <li className="navbarMenuItem">게시판</li>
+            <li className="navbarMenuItem">갤러리</li>
+          </ul>
         </div>
+        <div className="navarIconbox">
+          <FaSearch className="navarIcon" />
+          <FaUserAlt className="navarIcon" />
+          <FaShoppingCart className="navarIcon" />
+        </div>
+        <FaBars className="navbarToggleBtn" />
       </div>
-    </div>
+    </nav>
   );
 };
 
 export default Nav;
+
+// const Navbar = () => {
+//   const [drop, setDrop] = useState(false);
+
+//   return (
+//     <>
+//       <nav className="navbar2">
+//         <div to="/" className="navbarLogo2">
+//           ToWeStory
+//         </div>
+//         <ul>
+//           <li className="itemName">
+//             <div>ㅇㅎ</div>
+//           </li>
+//           <li className="itemName">
+//             <div>ㅇㅎ</div>
+//           </li>
+//           <li className="itemName">
+//             <div>ㅇㅎ</div>
+//           </li>
+//           <li className="itemName">
+//             <div>ㅇㅎ</div>
+//           </li>
+//         </ul>
+//         <button className="btn">Sign UP</button>
+//       </nav>
+//       <ul className="serviceSub">
+//         <li className="serviceName">드랍박스</li>
+//       </ul>
+//     </>
+//   );
+// };
