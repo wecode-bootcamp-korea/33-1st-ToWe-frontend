6331da6cd7cbf511df62052bb36041e6c25f570e
yesharry <reerohh@gmail.com>
2022-05-31 20:40:53 +0900

    깃허브 사이더 오류 수정

--- src/pages/itemCart/ItemCart.js
+++ src/pages/itemCart/ItemCart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ItemCart = () => {
-  return <div></div>;
+  return <div />;
 };
 
 export default ItemCart;
--- src/pages/itemDetail/ItemDetail.js
+++ src/pages/itemDetail/ItemDetail.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ItemDetail = () => {
-  return <div></div>;
+  return <div />;
 };
 
 export default ItemDetail;
